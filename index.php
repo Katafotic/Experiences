6fc5824c4e1b9f9ee7732ba272e1aee607dd633e
Katafotic <62174444+Katafotic@users.noreply.github.com>
2020-07-24 18:35:34 +0300

    beta
    betavesion from Windows OpenServer

--- index.php
+++ index.php
@@ -5,14 +5,240 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Заячья нора.</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/neon_banner.css">
+    <link rel="stylesheet" href="css/topnav.css">
+    <!--  -->
+    <style>
+
+
+        /* Full-width input fields */
+        input[type=text], input[type=password] {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            box-sizing: border-box;
+        }
+
+        /* Set a style for all buttons */
+        button {
+           /*
+            background-color: #4CAF50;
+            width: 100%;
+           */
+            background-color: antiquewhite;
+            color: black;
+            padding: 5px 10px;
+            margin: 0px;
+            border: 1px;
+            cursor: pointer;
+            text-align: left;
+        }
+
+        button:hover {
+            opacity: 0.8;
+        }
+
+        /* Extra styles for the cancel button */
+        .cancelbtn {
+            width: auto;
+            padding: 10px 18px;
+            background-color: #f44336;
+        }
+
+        /* Center the image and position the close button */
+        .imgcontainer {
+            text-align: center;
+            margin: 24px 0 12px 0;
+            position: relative;
+        }
+
+        img.avatar {
+            width: 30%;
+            border-radius: 50%;
+        }
+
+        .container {
+            padding: 16px;
+        }
+
+        span.psw {
+            float: right;
+            padding-top: 16px;
+        }
+
+        /* The Modal (background) */
+        .modal {
+            display: none; /* Hidden by default */
+            position: fixed; /* Stay in place */
+            z-index: 1; /* Sit on top */
+            left: 0;
+            top: 0;
+            width: 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: auto; /* Enable scroll if needed */
+            background-color: rgb(0,0,0); /* Fallback color */
+            background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+            padding-top: 50px;
+        }
+
+        /* Modal Content/Box */
+        .modal-content {
+            background-color: #fefefe;
+            margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+            border: 1px solid #888;
+            width: 70%; /* Could be more or less, depending on screen size */
+        }
+
+        /* The Close Button (x) */
+        .close {
+            position: absolute;
+            right: 25px;
+            top: 0;
+            color: #000;
+            font-size: 35px;
+            font-weight: bold;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: red;
+            cursor: pointer;
+        }
+
+        /* Add Zoom Animation */
+        .animate {
+            -webkit-animation: animatezoom 0.6s;
+            animation: animatezoom 0.6s
+        }
+
+        @-webkit-keyframes animatezoom {
+            from {-webkit-transform: scale(0)}
+            to {-webkit-transform: scale(1)}
+        }
+
+        @keyframes animatezoom {
+            from {transform: scale(0)}
+            to {transform: scale(1)}
+        }
+
+        /* Change styles for span and cancel button on extra small screens */
+        @media screen and (max-width: 300px) {
+            span.psw {
+                display: block;
+                float: none;
+            }
+            .cancelbtn {
+                width: 100%;
+            }
+        }
+    </style>
+    <!--  -->
+
+
 </head>
-<body>
-<h1> Header "h1" in body </h1>
-<?php
-echo "It first programm. It Hello, World.";
-$dat =date("d.m.y");
-echo "Date is ", $dat;
-?>
+<body class="main">
+    <div class="container">
+        <header class="header">
+            <button onclick="document.getElementById('id01').style.display='block'">Login</button>
+            <span class="topnav">
+                <a class="active" href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#contact">Contact</a>
+                <input type="text" placeholder="Search.." { width:30%; text-align:right;}>
+            </span>
+        </header>
+        <div class="left_colon">IT IS LEFT COLON</div>
+        <div class="right_colon"><!-- IT IS RIGHT COLON -->
+            <div class="sign-wrap-3">
+                <span class="sign_word"><p>Здесь могла</p>быть реклама
+                    <p class="neon_banner" >Но кому это </p><span class="neon_banner">надо?</span>
+                </span>
+            </div>
+        </div>
+        <div>
+            <div class="center"><!-- CENTER -->
+                    <p> <a href="https://www.youtube.com/watch?v=B33qcyjYaGY">Альбом Ч/Б</a> группы <a href="http://www.pilot.spb.ru/">Пилот</a> хорошо подходит к статье. Для затравки одноименная песня из него.
+                        <p class="audio"><audio src="/audio/pilot_che_b.mp3" controls ></audio></p>
+                    </p>
+                    <p>Gravity (from Latin gravitas, meaning 'weight'), or gravitation, is a natural phenomenon by which all things with mass or energy—including planets, stars, galaxies, and even light—are brought toward (or gravitate toward) one another. On Earth, gravity gives weight to physical objects, and the Moon's gravity causes the ocean tides. The gravitational attraction of the original gaseous matter present in the Universe caused it to begin coalescing and forming stars and caused the stars to group together into galaxies, so gravity is responsible for many of the large-scale structures in the Universe. Gravity has an infinite range, although its effects become increasingly weaker as objects get further away.</p>
+                    <p>Gravity is most accurately described by the general theory of relativity (proposed by Albert Einstein in 1915), which describes gravity not as a force, but as a consequence of the curvature of spacetime caused by the uneven distribution of mass. The most extreme example of this curvature of spacetime is a black hole, from which nothing—not even light—can escape once past the black hole's event horizon. However, for most applications, gravity is well approximated by Newton's law of universal gravitation, which describes gravity as a force, which causes any two bodies to be attracted to each other, with the force proportional to the product of their masses and inversely proportional to the square of the distance between them. </p>
+                    <p>Gravity is the weakest of the four fundamental interactions of physics, approximately 1038 times weaker than the strong interaction, 1036 times weaker than the electromagnetic force and 1029 times weaker than the weak interaction. As a consequence, it has no significant influence at the level of subatomic particles. In contrast, it is the dominant interaction at the macroscopic scale, and is the cause of the formation, shape and trajectory (orbit) of astronomical bodies. </p>
+                    <p>The earliest instance of gravity in the Universe, possibly in the form of quantum gravity, supergravity or a gravitational singularity, along with ordinary space and time, developed during the Planck epoch (up to 10−43 seconds after the birth of the Universe), possibly from a primeval state, such as a false vacuum, quantum vacuum or virtual particle, in a currently unknown manner. Attempts to develop a theory of gravity consistent with quantum mechanics, a quantum gravity theory, which would allow gravity to be united in a common mathematical framework (a theory of everything) with the other three fundamental interactions of physics, are a current area of research. </p>
+                    <p>General relativity predicts that energy can be transported out of a system through gravitational radiation. Any accelerating matter can create curvatures in the space-time metric, which is how the gravitational radiation is transported away from the system. Co-orbiting objects can generate curvatures in space-time such as the Earth-Sun system, pairs of neutron stars, and pairs of black holes. Another astrophysical system predicted to lose energy in the form of gravitational radiation are exploding supernovae.
+                        The first indirect evidence for gravitational radiation was through measurements of the Hulse–Taylor binary in 1973. This system consists of a pulsar and neutron star in orbit around one another. Its orbital period has decreased since its initial discovery due to a loss of energy, which is consistent for the amount of energy loss due to gravitational radiation. This research was awarded the Nobel Prize in Physics in 1993.
+
+                        The first direct evidence for gravitational radiation was measured on 14 September 2015 by the LIGO detectors. The gravitational waves emitted during the collision of two black holes 1.3 billion-light years from Earth were measured. This observation confirms the theoretical predictions of Einstein and others that such waves exist. It also opens the way for practical observation and understanding of the nature of gravity and events in the Universe including the Big Bang. Neutron star and black hole formation also create detectable amounts of gravitational radiation. This research was awarded the Nobel Prize in physics in 2017.
+
+                        As of 2020, the gravitational radiation emitted by the Solar System is far too small to measure with current technology.
+                    </p>
+            </div>
+        </div>
+        <footer class="footer">
+            <div class="footer_message">© Бига Вячеслав, 2019 <p><a class="email_link" href="mailto:viacheslav.manpower@gmail.com" >&#128386;    viacheslav.manpower@gmail.com</a></p><p>Сайт сделан просто так, от балды. Все совпадения случайные.</p></div>
+            <span class="socialweb_colon">
+               <!--  <a href="https://www.facebook.com/"><img src="/img/facebook.svg" ></a>   -->
+                <a href="https://www.facebook.com/"> <img class="socialnetwork_buttons" src="/img/facebook.svg"></a>
+                <a href="https://twitter.com/"> <img class="socialnetwork_buttons" src="/img/twitter.svg"> </a>
+                <a href="https://github.com/"><img class="socialnetwork_buttons" src="/img/github.svg" ></a>
+            </span>
+        </footer>
+    </div>
+    <!--  <footer class="footer">Copyright</footer> -->
 </body>
+
+
+
+
+
+
+
+<!--  -->
+
+<!-- The Modal -->
+<div id="id01" class="modal">
+  <span onclick="document.getElementById('id01').style.display='none'"
+        class="close" title="Close Modal">&times;</span>
+
+    <!-- Modal Content -->
+    <form class="modal-content animate" action="/action_page.php">
+        <div class="imgcontainer">
+            <img src="/img/octocat.png" alt="/img/img_avatar2.png" class="avatar">
+        </div>
+
+        <div class="container">
+            <div class="login_username">
+                <label for="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required>
+            </div>
+
+            <div class="login_password">
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required>
+
+            </div>
+            <!--
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+            -->
+            <button type="submit">Login</button>
+            <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+            </label>
+        </div>
+
+        <div class="container" style="background-color:#f1f1f1">
+            <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+            <span class="psw">Forgot <a href="#">password?</a></span>
+        </div>
+    </form>
+</div>
+<!--  -->
 </html>
